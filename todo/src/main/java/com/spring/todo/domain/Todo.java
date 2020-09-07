2fb9fea59ee9fff1c5c33b62040a86f1931277bd
yangseungbin <tanja1206@naver.com>
2020-09-07 19:29:49 +0900

    todo model

---
+++ todo/src/main/java/com/spring/todo/domain/Todo.java
@@ -0,0 +1,46 @@
+package com.spring.todo.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table
+public class Todo implements Serializable {
+    private static final long serialVersionUID = -947585423656694361L;
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String content;
+
+    @Column
+    private LocalDateTime createdDateTime;
+
+    @Column
+    private Boolean isComplete;
+
+    @Builder
+    public Todo(Long id, String content, LocalDateTime createdDateTime, Boolean isComplete) {
+        this.id = id;
+        this.content = content;
+        this.createdDateTime = createdDateTime;
+        this.isComplete = isComplete;
+    }
+
+    @Override
+    public String toString() {
+        return "Todo [id = " + id + ", content = " + content + ", createDateTime = " + createdDateTime + ", isComplete = " + isComplete + "]";
+    }
+}

9fc63906292275fd8ad6d1b5c275f66662d2a995
yangseungbin <tanja1206@naver.com>
2020-09-10 15:59:25 +0900

    not found

---
+++ auth/src/main/java/com/spring/auth/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.spring.auth.exception;
+
+public class UserNotFoundException extends BusinessException {
+    public UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+}

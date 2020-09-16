868bde000d8bdb58f90b2fc742fa1ee762e641d3
yangseungbin <tanja1206@naver.com>
2020-09-16 22:44:55 +0900

    post service impl

---
+++ auth/src/main/java/com/spring/auth/service/post/PostServiceImpl.java
@@ -0,0 +1,30 @@
+package com.spring.auth.service.post;
+
+import com.spring.auth.domain.request.PostWriteRequest;
+import com.spring.auth.entity.Post;
+import com.spring.auth.entity.repository.PostRepository;
+import com.spring.auth.security.AuthenticationFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+    private final PostRepository postRepository;
+    private final AuthenticationFacade authenticationFacade;
+
+    @Override
+    public void write(PostWriteRequest postWriteRequest) {
+        String title = postWriteRequest.getTitle();
+        String content = postWriteRequest.getContent();
+        String email = authenticationFacade.getUserEmail();
+
+        postRepository.save(
+            Post.builder()
+                .title(title)
+                .content(content)
+                .email(email)
+                .build()
+        );
+    }
+}

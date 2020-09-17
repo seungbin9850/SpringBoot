5473b4ca92d7b2cb412aec9700aeb08b058bfcef
yangseungbin <tanja1206@naver.com>
2020-09-17 15:16:16 +0900

    post controller

--- auth/src/main/java/com/spring/auth/controller/PostController.java
+++ auth/src/main/java/com/spring/auth/controller/PostController.java
@@ -1,11 +1,13 @@
 package com.spring.auth.controller;
 
 import com.spring.auth.domain.request.PostWriteRequest;
+import com.spring.auth.entity.Post;
 import com.spring.auth.service.post.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/post")
@@ -16,5 +18,15 @@ public class PostController {
     @PostMapping
     public void write(@RequestBody @Valid PostWriteRequest postWriteRequest) {
         postService.write(postWriteRequest);
+    }
+
+    @GetMapping
+    public List<Post> show() {
+        return postService.showAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        postService.delete(id);
     }
 }

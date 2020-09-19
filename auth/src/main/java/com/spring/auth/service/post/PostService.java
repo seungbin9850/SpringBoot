d11351e94852b8a00bb84c1ab6d767324e7824cf
yangseungbin <tanja1206@naver.com>
2020-09-19 22:05:33 +0900

    post service

--- auth/src/main/java/com/spring/auth/service/post/PostService.java
+++ auth/src/main/java/com/spring/auth/service/post/PostService.java
@@ -4,9 +4,11 @@ import com.spring.auth.domain.request.PostWriteRequest;
 import com.spring.auth.entity.Post;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostService {
     void write(PostWriteRequest postWriteRequest);
     void delete(Long id);
     List<Post> showAll();
+    Optional<Post> showOne(Long id);
 }
